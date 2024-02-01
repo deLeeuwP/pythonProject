d7ca7f63e0bc8601ceb25cf9e3b13ec25f3a08cf
deleeuwP <p.deleeuw@liferadiopharma.com>
2024-02-01 14:24:35 +0100

    Django Tutorial part 5, wrote a bug test for putting a "recently" tag on Questions posted in the future.

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,18 @@
+import datetime
+
 from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
 
 # Create your tests here.
+class QuestionModelTests(TestCase):
+    def test_was_published_in_future(self):
+        """
+        was_published_recently() should return False for Questions posted in the future.
+
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        fq = Question(pub_date=time)
+        self.assertIs(fq.was_published_recently(),False)
